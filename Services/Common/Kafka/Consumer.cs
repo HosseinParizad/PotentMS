1584336a867d7fd426abc53e0ff71a5bd4ea7c6f
HosseinParizad <Parizad50@gmail.com>
2021-03-28 08:44:25 +1100

    add iTodo project

--- Services/Common/Kafka/Consumer.cs
+++ Services/Common/Kafka/Consumer.cs
@@ -1,53 +1,151 @@
 using System;
 using System.Threading;
 using Confluent.Kafka;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
-public class Consumer
+
+namespace KafkaHelper
 {
-    public static Listen(string topic)
+    public class Consumer
     {
-        var conf = new ConsumerConfig
-        { 
-            GroupId = "test-consumer-group",
-            BootstrapServers = "localhost:9092",
-            // Note: The AutoOffsetReset property determines the start offset in the event
-            // there are not yet any committed offsets for the consumer group for the
-            // topic/partitions of interest. By default, offsets are committed
-            // automatically, so in this example, consumption will only start from the
-            // earliest message in the topic 'my-topic' the first time you run the program.
-            AutoOffsetReset = AutoOffsetReset.Earliest
-        };
-
-        using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+        public Consumer(string brokerList, List<string> topics, CancellationToken cancellationToken, Action<string> onMessageReceived)
         {
-            c.Subscribe(topic);
-
-             CancellationTokenSource cts = new CancellationTokenSource();
-             Console.CancelKeyPress += (_, e) => {
-                 e.Cancel = true; // prevent the process from terminating.
-                 cts.Cancel();
-             };
-
-            try
+            if (string.IsNullOrEmpty(brokerList))
             {
-                while (true)
+                throw new ArgumentException($"'{nameof(brokerList)}' cannot be null or empty.", nameof(brokerList));
+            }
+
+            if (topics is null)
+            {
+                throw new ArgumentNullException(nameof(topics));
+            }
+
+            if (onMessageReceived is null)
+            {
+                throw new ArgumentNullException(nameof(onMessageReceived));
+            }
+            
+            BrokerList = brokerList;
+            Topics = topics;
+            CancellationToken = cancellationToken;
+            OnMessageReceived = onMessageReceived;
+            Listen();
+        }
+
+        void Listen()
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = BrokerList,
+                GroupId = "csharp-consumer",
+                EnableAutoCommit = false,
+                StatisticsIntervalMs = 5000,
+                SessionTimeoutMs = 6000,
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnablePartitionEof = true,
+                // A good introduction to the CooperativeSticky assignor and incremental rebalancing:
+                // https://www.confluent.io/blog/cooperative-rebalancing-in-kafka-streams-consumer-ksqldb/
+                PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky
+            };
+
+            const int commitPeriod = 5;
+
+            // Note: If a key or value deserializer is not set (as is the case below), the 
+            // deserializer corresponding to the appropriate type from Confluent.Kafka.Deserializers
+            // will be used automatically (where available). The default deserializer for string
+            // is UTF8. The default deserializer for Ignore returns null for all input data
+            // (including non-null data).
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config)
+                // Note: All handlers are called on the main .Consume thread.
+                .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+                //.SetStatisticsHandler((_, json) => Console.WriteLine($"Statistics: {json}"))
+                .SetPartitionsAssignedHandler((c, partitions) =>
                 {
-                    try
+                    // Since a cooperative assignor (CooperativeSticky) has been configured, the
+                    // partition assignment is incremental (adds partitions to any existing assignment).
+                    Console.WriteLine($"Incremental partition assignment: [{string.Join(", ", partitions)}]");
+
+                    // Possibly manually specify start offsets by returning a list of topic/partition/offsets
+                    // to assign to, e.g.:
+                    // return partitions.Select(tp => new TopicPartitionOffset(tp, externalOffsets[tp]));
+                })
+                .SetPartitionsRevokedHandler((c, partitions) =>
+                {
+                    // Since a cooperative assignor (CooperativeSticky) has been configured, the revoked
+                    // assignment is incremental (may remove only some partitions of the current assignment).
+                    Console.WriteLine($"Incremental partition revokation: [{string.Join(", ", partitions)}]");
+                })
+                .SetPartitionsLostHandler((c, partitions) =>
+                {
+                    // The lost partitions handler is called when the consumer detects that it has lost ownership
+                    // of its assignment (fallen out of the group).
+                    Console.WriteLine($"Partitions were lost: [{string.Join(", ", partitions)}]");
+                })
+                .Build())
+            {
+                consumer.Subscribe(Topics);
+
+                try
+                {
+                    while (true)
                     {
-                        var cr = c.Consume(cts.Token);
-                        Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
-                    }
-                    catch (ConsumeException e)
-                    {
-                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        try
+                        {
+                            var consumeResult = consumer.Consume(CancellationToken);
+
+                            if (consumeResult.IsPartitionEOF)
+                            {
+                                Console.WriteLine(
+                                    $"Reached end of topic {consumeResult.Topic}, partition {consumeResult.Partition}, offset {consumeResult.Offset}.");
+
+                                continue;
+                            }
+
+                            OnMessageReceived(consumeResult.Message.Value);
+                            Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Message.Value}");
+
+                            if (consumeResult.Offset % commitPeriod == 0)
+                            {
+                                // The Commit method sends a "commit offsets" request to the Kafka
+                                // cluster and synchronously waits for the response. This is very
+                                // slow compared to the rate at which the consumer is capable of
+                                // consuming messages. A high performance application will typically
+                                // commit offsets relatively infrequently and be designed handle
+                                // duplicate messages in the event of failure.
+                                try
+                                {
+                                    consumer.Commit(consumeResult);
+                                }
+                                catch (KafkaException e)
+                                {
+                                    Console.WriteLine($"Commit error: {e.Error.Reason}");
+                                }
+                            }
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Consume error: {e.Error.Reason}");
+                        }
                     }
                 }
-            }
-            catch (OperationCanceledException)
-            {
-                // Ensure the consumer leaves the group cleanly and final offsets are committed.
-                c.Close();
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Closing consumer.");
+                    consumer.Close();
+                }
             }
         }
+
+        #region prop
+
+        string BrokerList { get; }
+        List<string> Topics { get; }
+        CancellationToken CancellationToken { get; }
+        Action<string> OnMessageReceived { get; }
+
+        #endregion
     }
 }
---
+++ Services/iTodo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using KafkaHelper;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace iTodo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new CancellationTokenSource();
+            var token = source.Token;
+
+            var topics = new List<string>() { "task" };
+            var iTodoConsumer = new Consumer("localhost:9092", topics, token, MessageReceived);
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        static void MessageReceived(string message)
+        {
+            Console.WriteLine($"i can hear you {message}");
+        }
+    }
+}

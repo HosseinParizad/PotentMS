799fe161bd60baab82861eab1fde177cab8306cb
HosseinParizad <Parizad50@gmail.com>
2021-04-18 09:08:07 +1000

    make it work except test

--- __Test__/Services/iTodoTest/iTodo.feature
+++ __Test__/Services/iTodoTest/iTodo.feature
@@ -7,13 +7,13 @@ Feature: Add
 
 	Scenario: Task should order as enter be default
 		When I send the following task:
-			| TaskDesc                            | GroupKey |
+			| TaskDesc                            | GroupKey|
 			| Watch cat videos on YouTube all day | @me     |
 			| Do what ever you want               | @you    |
 			| Eat what ever you want              | @you    |
 			| check the reault :)                 | @you    |
 		Then I should see the following todo list:
-			| TaskDesc                            | GroupKey | Index |
+			| TaskDesc                            | GroupKey| Index |
 			| Watch cat videos on YouTube all day | @me     | 0     |
 			| Eat what ever you want              | @you    | 1     |
 			| check the reault :)                 | @you    | 2     |
@@ -26,7 +26,7 @@ Feature: Add
 	# 		| 125 | @me     | Watch dog videos on YouTube all day |
 	# 	And I send deadline '12/12/2021 12:34' for task id '123'
 	# 	Then I should see the following todo list:
-	# 		| Id  | TaskDesc                            | GroupKey | Deadline         | Index |
+	# 		| Id  | TaskDesc                            | GroupKey | Deadline        | Index |
 	# 		| 123 | Watch cat videos on YouTube all day | @me     | 12/12/2021 12:34 | 0     |
 	# 		| 125 | Watch dog videos on YouTube all day | @me     |                  | 1     |
 
@@ -36,6 +36,6 @@ Feature: Add
 	# 		| 123 | @me     | Watch cat videos on YouTube all day |
 	# 	And I send deadline '12/12/2010 12:34' for task id '123'
 	# 	Then I should see the following todo list:
-	# 		| Id  | TaskDesc                            | GroupKey | Deadline | Index |
+	# 		| Id  | TaskDesc                            | GroupKey| Deadline | Index |
 	# 		| 123 | Watch cat videos on YouTube all day | @me     |          | 0     |
 	# 		| 125 | Watch dog videos on YouTube all day | @me     |          | 1     |
--- __Test__/Services/iTodoTest/iTodoStepDefinitions.cs
+++ __Test__/Services/iTodoTest/iTodoStepDefinitions.cs
@@ -6,6 +6,8 @@ using System.Text.Json;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
 
 namespace SpecFlowDemo.Steps
 {
@@ -32,12 +34,53 @@ namespace SpecFlowDemo.Steps
                 var content = new iTodo() { Description = row["TaskDesc"] };
                 var msg = new Msg() { Action = "newTask", GroupKey = row["GroupKey"], Content = JsonSerializer.Serialize(content) };
                 var dataToSend = JsonSerializer.Serialize(msg);
-                HttpMakeARequest(table, url, httpMethod, dataToSend);
+                HttpMakeARequest(url, httpMethod, dataToSend);
             }
 
         }
 
-        private static void HttpMakeARequest(Table table, string url, HttpMethod httpMethod, string dataToSend)
+        [Then("I should see the following todo list:")]
+        public void ThenTheResultShouldBe(TechTalk.SpecFlow.Table table)
+        {
+            dynamic[] todos = null;
+            //foreach (var row in table.Rows)
+            //{
+            //    var url = $"https://localhost:5003/TodoQuery?groupKey={row["GroupKey"]}";
+            //    todos = MakeAGetRequest(url);
+            //}
+
+            var url = "https://localhost:5003/TodoQuery?groupKey=All";
+            todos = MakeAGetRequest(url);
+            Assert.AreEqual(todos.Select(l => l.GetProperty("description").ToString()).ToArray(), table.Rows.Select(r => r["TaskDesc"]).ToArray());
+            //Assert.AreEqual(todos, "Do what you want");
+        }
+
+        static dynamic[] MakeAGetRequest(string url)
+        {
+            dynamic[] todos = null;
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+            using (var httpClient = new HttpClient(handler, false))
+            {
+                using (var request = new HttpRequestMessage(HttpMethod.Get, url))
+                {
+                    try
+                    {
+                        var response = httpClient.Send(request);
+                        var responseString = response.Content.ReadAsStringAsync().Result;
+                        todos = JsonSerializer.Deserialize<dynamic[]>(responseString);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            return todos;
+        }
+
+        static void HttpMakeARequest(string url, HttpMethod httpMethod, string dataToSend)
         {
             var handler = new HttpClientHandler();
             handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
@@ -57,56 +100,6 @@ namespace SpecFlowDemo.Steps
             }
         }
 
-        [Then("I should see the following todo list:")]
-        public void ThenTheResultShouldBe(TechTalk.SpecFlow.Table table)
-        {
-            dynamic todos = table;
-            var handler = new HttpClientHandler();
-            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
-
-            foreach (var row in table.Rows)
-            {
-                var r = row["GroupKey"];
-                using (var httpClient = new HttpClient(handler, false))
-                {
-                    using (var request = new HttpRequestMessage(HttpMethod.Get, $"https://localhost:5003/TodoQuery?groupKey={r}"))
-                    {
-                        //request.Headers.TryAddWithoutValidation("Accept", "application/json");
-
-                        //request.Content = new StringContent("", Encoding.UTF8, "application/json");
-                        try
-                        {
-                            var response = httpClient.Send(request);
-                            var responseString = response.Content.ReadAsStringAsync().Result;
-                            //todos = JsonSerializer.Deserialize<object>(responseString);
-                            todos = JsonSerializer.Deserialize<dynamic[]>(responseString);
-                            //dynamic jsonResponse = JsonConvert.DeserializeObject(responseString);
-                            //var pet = JsonSerializer.Deserialize<Pet>(responseString);
-
-                        }
-                        catch
-                        {
-                        }
-
-
-                        //         //Assert.Null(r.StatusCode);
-                        //         var responseString = response.Content.ReadAsStringAsync().Result;
-                        //         todos = JsonSerializer.Deserialize<dynamic[]>(responseString);
-                        //         //Assert.AreEqual(belongTo, d[0].GetProperty("assignedTo").ToString());
-                        //         //Assert.AreEqual(desc, d[0].GetProperty("description").ToString());
-                    }
-                }
-            }
-            //foreach (var row in table.Rows)
-            //{
-            //    Assert.AreEqual(true, todos.Single(t=> t.Description == row["TaskDesc"]))
-            //}
-            //Assert.AreEqual(todos[0].description,"");
-            //RestHelper.GetObjects<dynamic>("");
-            //var url = new Uri(r)
-            //var myObject = await RestClient.GetObjects<dynamic>(url);
-
-        }
     }
 
     internal class Msg
@@ -126,5 +119,26 @@ namespace SpecFlowDemo.Steps
         {
         }
         public string Description { get; set; }
+    }
+
+    class expectedResultItem
+    {
+        string Id { get; }
+        public string Description { get; set; }
+        public string GroupKey { get; set; }
+        public string AssignedTo { get; set; }
+        public List<string> Category { get; set; }
+        public DateTime? Deadline { get; set; }
+        public int Sequence { get; set; }
+
+        public override string ToString()
+        {
+            return $"GroupKey: {GroupKey}";
+        }
+    }
+
+    class expectedResult
+    {
+        public List<expectedResultItem> description { get; set; }
     }
 }
